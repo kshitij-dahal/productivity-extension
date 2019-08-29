fc72b1fceda642f494eafae29bddff193c2d8776
kshitij314 <kshitijdahal314@gmail.com>
2019-08-29 14:26:35 -0400

    timer is displayed on popup; buttons do not function yet on popup

--- background.js
+++ background.js
@@ -1,18 +1,51 @@
 // establish connection with popup if it is open
+chrome.runtime.onInstalled.addListener(function(info) {
+  if (info.reason == "install" || info.reason == "update") {
+    chrome.storage.sync.set({
+      btn_text: "START",
+      timer_min: 0,
+      timer_sec: 0
+    });
+  }
+});
+var local_min;
+chrome.storage.sync.get("timer_min", function(result) {
+  local_min = result.timer_min;
+});
+var local_sec;
+chrome.storage.sync.get("timer_sec", function(result) {
+  local_sec = result.timer_sec;
+});
 
-function establishLongConnection() {
+function establish_long_connection() {
   var port = chrome.runtime.connect({ name: "timer_request" });
+  port.postMessage({ timer_min: local_min, timer_sec: local_sec });
   port.onMessage.addListener(function(msg) {
     alert(msg.msg);
+    var interval = setInterval(run_timer, 1000);
   });
   port.onDisconnect.addListener(function() {
     alert("disconnected");
   });
 }
 
+function run_timer() {
+  if (local_sec == 59) {
+    local_min++;
+    local_sec = 0;
+  } else {
+    local_sec++;
+  }
+  console.log(local_min, local_sec);
+}
+
+function send_timer_info(port) {
+  port.postMessage({ timer_min: local_min, timer_sec: local_sec });
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.msg == "pop") {
     // alert("its open");
-    establishLongConnection();
+    establish_long_connection();
   }
 });
--- popup.js
+++ popup.js
@@ -11,10 +11,44 @@ chrome.runtime.onConnect.addListener(function(port) {
   });
   */
 
+var timer_min;
+var timer_sec;
 chrome.runtime.sendMessage({ msg: "pop" });
 chrome.runtime.onConnect.addListener(function(port) {
   port.postMessage({ msg: "hello man" });
+  port.onMessage.addListener(function(msg) {
+    timer_min = msg.timer_min;
+    timer_sec = msg.timer_sec;
+    console.log(timer_min + ":" + timer_sec);
+    setInterval(update_timer, 1000);
+  });
 });
+
+var timer_text;
+function update_timer() {
+  timer_text = document.querySelectorAll("span");
+  var min_text, sec_text;
+  if (timer_min < 10) {
+    min_text = document.createTextNode("0" + timer_min);
+  } else {
+    min_text = document.createTextNode(timer_min);
+  }
+  if (timer_sec < 10) {
+    sec_text = document.createTextNode("0" + timer_sec);
+  } else {
+    sec_text = document.createTextNode(timer_sec);
+  }
+  timer_text[1].removeChild(timer_text[1].firstChild);
+  timer_text[3].removeChild(timer_text[3].firstChild);
+  timer_text[1].appendChild(min_text);
+  timer_text[3].appendChild(sec_text);
+  if (timer_sec == 59) {
+    timer_min++;
+    timer_sec = 0;
+  } else {
+    timer_sec++;
+  }
+}
 
 var btn_clicked = document.querySelector("button");
 btn_clicked.addEventListener("click", change_btn_text, false);
@@ -28,4 +62,5 @@ function change_btn_text() {
   } else {
     btn_text.innerHTML = "PAUSE";
   }
+  chrome.storage.sync.set({ btn_text: btn_text.innerHTML });
 }
