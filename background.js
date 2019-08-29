449edc22a902db1fd51ccd33a625ce96b095e930
kshitij314 <kshitijdahal314@gmail.com>
2019-08-29 09:45:20 -0400

    connection between background and popup established to send messages; background detects when popup is opened or closed

---
+++ background.js
@@ -0,0 +1,18 @@
+// establish connection with popup if it is open
+
+function establishLongConnection() {
+  var port = chrome.runtime.connect({ name: "timer_request" });
+  port.onMessage.addListener(function(msg) {
+    alert(msg.msg);
+  });
+  port.onDisconnect.addListener(function() {
+    alert("disconnected");
+  });
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.msg == "pop") {
+    alert("its open");
+    establishLongConnection();
+  }
+});
--- popup.js
+++ popup.js
@@ -1,1 +1,17 @@
-// events to be handled for popup.html
+// connect with background.js to receive timer information
+/*var port = chrome.runtime.connect({ name: "timer_request" });
+port.onMessage.addListener(function(msg) {
+  var timer_min = msg.getMinutes();
+  var timer_sec = msg.getSeconds();
+  alert(timer_min);
+  alert(timer_sec);
+});
+chrome.runtime.onConnect.addListener(function(port) {
+    port.postMessage(new Date());
+  });
+  */
+
+chrome.runtime.sendMessage({ msg: "pop" });
+chrome.runtime.onConnect.addListener(function(port) {
+  port.postMessage({ msg: "hello man" });
+});
