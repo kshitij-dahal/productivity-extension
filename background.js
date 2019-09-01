fe17985526d2ae5df1250624d709bf1358dc3fed
kshitij314 <kshitijdahal314@gmail.com>
2019-09-01 15:38:17 -0400

    reset timer at first time opening extension in the day

--- background.js
+++ background.js
@@ -6,10 +6,12 @@ var interval;
 // upon first install, store 00:00 for value of timer in the storage
 function store_initial_timer_values(info) {
   if (info.reason == "install" || info.reason == "update") {
+    var today = new Date();
     chrome.storage.sync.set({
       btn_text: "START",
       timer_min: 0,
-      timer_sec: 0
+      timer_sec: 0,
+      current_date: today
     });
   }
 }
@@ -18,6 +20,7 @@ function store_initial_timer_values(info) {
 // open a long-lived channel to popup.js and send current timer value,
 // then start the timer
 function establish_long_connection(request, sender) {
+  //check_if_new_day();
   if (request.msg == "popup_open") {
     var port = chrome.runtime.connect({ name: "timer_request" });
     port.postMessage({
@@ -40,7 +43,6 @@ function establish_long_connection(request, sender) {
         console.log(result.btn_text);
       });
     });
-  } else {
   }
 }
 
@@ -79,3 +81,33 @@ chrome.storage.sync.get("btn_text", function(result) {
   local_btn_text = result.btn_text;
 });
 chrome.storage.onChanged.addListener(configure_timer);
+
+function check_if_new_day() {
+  var today_date = new Date();
+  var stored_date;
+  chrome.storage.sync.get("current_date", function(result) {
+    stored_date = result.current_date;
+  });
+
+  if (
+    !(
+      today_date.getDate() == stored_date.getDate() &&
+      today_date.getMonth() == stored_date.getMonth() &&
+      today_date.getFullYear() == stored_date.getFullYear()
+    )
+  ) {
+    /*
+    MISSING: if decided to add storing feature, include the storage of current time into database here 
+    */
+    // they all dont match thus it is the first time being opened today
+    local_min = 0;
+    local_sec = 0;
+    local_btn_text = "START";
+    chrome.storage.sync.set({
+      btn_text: "START",
+      timer_min: 0,
+      timer_sec: 0,
+      current_date: today
+    });
+  }
+}
